12c67fb3b639abb5ca3fbf031151fc38afb9ad7e
Rizkifreao <rizkipebrianto96@gmail.com>
2019-08-19 12:57:53 +0700

    update

--- sinta.sql
+++ sinta.sql
@@ -16,40 +16,6 @@
 CREATE DATABASE IF NOT EXISTS `sinta` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `sinta`;
 
--- Dumping structure for table sinta.detail_pesanan
-CREATE TABLE IF NOT EXISTS `detail_pesanan` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pemesanan_id` int(11) NOT NULL DEFAULT '0',
-  `no_kontainer` varchar(50) NOT NULL DEFAULT '0',
-  `no_seal` varchar(50) NOT NULL DEFAULT '0',
-  `biaya_tambahan` int(11) NOT NULL DEFAULT '0',
-  `total` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `no_kontainer` (`no_kontainer`),
-  KEY `FK_detail_pesanan_pemesanan` (`pemesanan_id`),
-  CONSTRAINT `FK_detail_pesanan_pemesanan` FOREIGN KEY (`pemesanan_id`) REFERENCES `pemesanan` (`id_pesanan`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
-
--- Dumping data for table sinta.detail_pesanan: ~9 rows (approximately)
-/*!40000 ALTER TABLE `detail_pesanan` DISABLE KEYS */;
-INSERT IGNORE INTO `detail_pesanan` (`id`, `pemesanan_id`, `no_kontainer`, `no_seal`, `biaya_tambahan`, `total`) VALUES
-	(5, 3, '1234567', '1234567', 20000, 0),
-	(6, 3, '1234564', '1234567', 20000, 0),
-	(7, 3, '1234563', '1234567', 20000, 0),
-	(8, 5, '6666666', '6666666', 900000, 0),
-	(9, 8, 'asd1111', 'asd1111', 550000, 0),
-	(10, 2, 'asd1112', 'asd1112', 500000, 0),
-	(12, 6, 'asdasd3', 'ddd', 2000, 0),
-	(13, 7, 'ccccccc', 'ccccccc', -1222, 0),
-	(14, 7, 'aaaaaaa', 'aaaaaaa', 222, 0),
-	(15, 7, 'bbbbbbb', 'bbbbbbb', -111, 0),
-	(16, 7, 'wwwwwww', 'wwwwwww', 111, 0),
-	(17, 9, '9999999', '9999999', 200000, 0),
-	(18, 9, '7657465', '7657465', 200000, 0),
-	(19, 10, 'abcabca', 'abc123', 200000, 0),
-	(20, 10, 'acvacva', 'acv555', 200000, 0);
-/*!40000 ALTER TABLE `detail_pesanan` ENABLE KEYS */;
-
 -- Dumping structure for table sinta.konsumen
 CREATE TABLE IF NOT EXISTS `konsumen` (
   `id_konsumen` int(11) NOT NULL AUTO_INCREMENT,
@@ -66,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `konsumen` (
 
 -- Dumping data for table sinta.konsumen: ~2 rows (approximately)
 /*!40000 ALTER TABLE `konsumen` DISABLE KEYS */;
-INSERT IGNORE INTO `konsumen` (`id_konsumen`, `nama_konsumen`, `jabatan`, `perusahaan`, `npwp`, `alamat`, `profil_perusahaan`, `dok_MOU`, `is_delete`) VALUES
+INSERT INTO `konsumen` (`id_konsumen`, `nama_konsumen`, `jabatan`, `perusahaan`, `npwp`, `alamat`, `profil_perusahaan`, `dok_MOU`, `is_delete`) VALUES
 	(7, 'Bayung Pahlihan', 'General Manager', 'PT. Escorindo Jasa Prima', '02.021.481.3-046.000', 'Wisma Mitra Sunter Lt 4-01 Yos Sudarso 89 Blok C-1', NULL, '7-MOU.pdf', 0),
 	(8, 'ASF', 'SAD', 'awd', 'awd', 'adaw', 'ad', '8-MOU.pdf', 0),
 	(9, 'Rizki', 'Manager', 'PT ABC', '222 222 3333 5555', 'Jl Bumi Asri', NULL, NULL, 0);
@@ -83,9 +49,9 @@ CREATE TABLE IF NOT EXISTS `notifikasi` (
   PRIMARY KEY (`id_notif`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
--- Dumping data for table sinta.notifikasi: ~13 rows (approximately)
+-- Dumping data for table sinta.notifikasi: ~12 rows (approximately)
 /*!40000 ALTER TABLE `notifikasi` DISABLE KEYS */;
-INSERT IGNORE INTO `notifikasi` (`id_notif`, `pengirim`, `penerima`, `keterangan`, `data`, `status_notif`) VALUES
+INSERT INTO `notifikasi` (`id_notif`, `pengirim`, `penerima`, `keterangan`, `data`, `status_notif`) VALUES
 	(1, 25, 1, 'Telah mengajukan pesanan', 14, 'TRUE'),
 	(2, 25, 1, 'Telah mengajukan pesanan', 15, 'TRUE'),
 	(3, 25, 23, 'Telah mengajukan pesanan', 15, 'FALSE'),
@@ -125,9 +91,9 @@ CREATE TABLE IF NOT EXISTS `pemesanan` (
   KEY `FK_pemesanan_konsumen` (`id_konsumen`)
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
--- Dumping data for table sinta.pemesanan: ~14 rows (approximately)
+-- Dumping data for table sinta.pemesanan: ~13 rows (approximately)
 /*!40000 ALTER TABLE `pemesanan` DISABLE KEYS */;
-INSERT IGNORE INTO `pemesanan` (`id_pesanan`, `id_konsumen`, `konsumen`, `nama_barang`, `kapasitas_muat`, `tujuan`, `jum_kontainer`, `tipe`, `_20`, `_40`, `tarif`, `total_tarif`, `tgl_pesan`, `jadwal_kirim`, `keterangan`, `status_pengiriman`, `is_tagihan`, `send_by`) VALUES
+INSERT INTO `pemesanan` (`id_pesanan`, `id_konsumen`, `konsumen`, `nama_barang`, `kapasitas_muat`, `tujuan`, `jum_kontainer`, `tipe`, `_20`, `_40`, `tarif`, `total_tarif`, `tgl_pesan`, `jadwal_kirim`, `keterangan`, `status_pengiriman`, `is_tagihan`, `send_by`) VALUES
 	(2, 8, 'Bayung Pahlihan', 'asd', '2 Kg', 'Cilacap', 2, '', 1800000, 0, 1800000, 3600000, '2019-07-14', '2019-07-14', 'adwaw', 'PROSES', 0, NULL),
 	(3, 7, 'Bayung Pahlihan', 'Barang1', '20 Ton', 'Cilacap', 3, '20\'', 1800000, 0, 1800000, 5400000, '2019-07-14', '2019-07-14', 'ASD', 'SELESAI', 1, NULL),
 	(4, 7, 'Bayung Pahlihan', 'AKU', '20 Ton', 'Narogong', 1, '20\'', 1800000, 0, 1800000, 0, '2019-07-14', '2019-07-15', 'asd', 'PROSES', 0, NULL),
@@ -144,6 +110,40 @@ INSERT IGNORE INTO `pemesanan` (`id_pesanan`, `id_konsumen`, `konsumen`, `nama_b
 	(17, 9, 'PT ABC', 'kk', '20 Ton', 'BDG', 1, '20\'', 1500000, 0, 1500000, 1500000, '2019-08-18', '2019-08-19', 'kk', 'PENDING', 0, 25);
 /*!40000 ALTER TABLE `pemesanan` ENABLE KEYS */;
 
+-- Dumping structure for table sinta.detail_pesanan
+CREATE TABLE IF NOT EXISTS `detail_pesanan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pemesanan_id` int(11) NOT NULL DEFAULT '0',
+  `no_kontainer` varchar(50) NOT NULL DEFAULT '0',
+  `no_seal` varchar(50) NOT NULL DEFAULT '0',
+  `biaya_tambahan` int(11) NOT NULL DEFAULT '0',
+  `total` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `no_kontainer` (`no_kontainer`),
+  KEY `FK_detail_pesanan_pemesanan` (`pemesanan_id`),
+  CONSTRAINT `FK_detail_pesanan_pemesanan` FOREIGN KEY (`pemesanan_id`) REFERENCES `pemesanan` (`id_pesanan`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table sinta.detail_pesanan: ~9 rows (approximately)
+/*!40000 ALTER TABLE `detail_pesanan` DISABLE KEYS */;
+INSERT INTO `detail_pesanan` (`id`, `pemesanan_id`, `no_kontainer`, `no_seal`, `biaya_tambahan`, `total`) VALUES
+	(5, 3, '1234567', '1234567', 20000, 0),
+	(6, 3, '1234564', '1234567', 20000, 0),
+	(7, 3, '1234563', '1234567', 20000, 0),
+	(8, 5, '6666666', '6666666', 900000, 0),
+	(9, 8, 'asd1111', 'asd1111', 550000, 0),
+	(10, 2, 'asd1112', 'asd1112', 500000, 0),
+	(12, 6, 'asdasd3', 'ddd', 2000, 0),
+	(13, 7, 'ccccccc', 'ccccccc', -1222, 0),
+	(14, 7, 'aaaaaaa', 'aaaaaaa', 222, 0),
+	(15, 7, 'bbbbbbb', 'bbbbbbb', -111, 0),
+	(16, 7, 'wwwwwww', 'wwwwwww', 111, 0),
+	(17, 9, '9999999', '9999999', 200000, 0),
+	(18, 9, '7657465', '7657465', 200000, 0),
+	(19, 10, 'abcabca', 'abc123', 200000, 0),
+	(20, 10, 'acvacva', 'acv555', 200000, 0);
+/*!40000 ALTER TABLE `detail_pesanan` ENABLE KEYS */;
+
 -- Dumping structure for table sinta.rute
 CREATE TABLE IF NOT EXISTS `rute` (
   `id_rute` int(11) NOT NULL AUTO_INCREMENT,
@@ -157,7 +157,7 @@ CREATE TABLE IF NOT EXISTS `rute` (
 
 -- Dumping data for table sinta.rute: ~4 rows (approximately)
 /*!40000 ALTER TABLE `rute` DISABLE KEYS */;
-INSERT IGNORE INTO `rute` (`id_rute`, `id_konsumen`, `tujuan`, `_20`, `_40`, `is_deleted`) VALUES
+INSERT INTO `rute` (`id_rute`, `id_konsumen`, `tujuan`, `_20`, `_40`, `is_deleted`) VALUES
 	(12, 7, 'Narogong', 1800000, 0, 0),
 	(13, 7, 'Cilacap', 0, 7900000, 0),
 	(14, 9, 'BDG', 1500000, 0, 0),
@@ -183,7 +183,7 @@ CREATE TABLE IF NOT EXISTS `sistem_setting` (
 
 -- Dumping data for table sinta.sistem_setting: ~1 rows (approximately)
 /*!40000 ALTER TABLE `sistem_setting` DISABLE KEYS */;
-INSERT IGNORE INTO `sistem_setting` (`id`, `nama_perusahaan`, `alamat`, `telp`, `email`, `bank`, `cabang_bank`, `no_rekening`, `atas_nama`, `npwp`, `direktur`, `logo`) VALUES
+INSERT INTO `sistem_setting` (`id`, `nama_perusahaan`, `alamat`, `telp`, `email`, `bank`, `cabang_bank`, `no_rekening`, `atas_nama`, `npwp`, `direktur`, `logo`) VALUES
 	(0, 'CV. MAJU BERSAMA SEJAHTERA', 'Jl. Setrawangi IV No 24 Babakan Surabaya Kiaracondong Bandung 40215\r\n', '(022) 20527470', 'mbsmajubersama@gmail.com', 'MANDIRI', 'BANDUNG', '131-00-1383634-3', 'CV.MAJU BERSAMA', '74 309 766 9 424 000', 'IMAN HERI ALFIANSYAH, S.SOS', 'Logo.png');
 /*!40000 ALTER TABLE `sistem_setting` ENABLE KEYS */;
 
@@ -197,7 +197,7 @@ CREATE TABLE IF NOT EXISTS `tb_groups` (
 
 -- Dumping data for table sinta.tb_groups: ~2 rows (approximately)
 /*!40000 ALTER TABLE `tb_groups` DISABLE KEYS */;
-INSERT IGNORE INTO `tb_groups` (`id`, `name`, `description`) VALUES
+INSERT INTO `tb_groups` (`id`, `name`, `description`) VALUES
 	(1, 'Admin', 'Roles Admin'),
 	(2, 'Konsumen', 'Roles untuk user Klien');
 /*!40000 ALTER TABLE `tb_groups` ENABLE KEYS */;
@@ -243,7 +243,7 @@ CREATE TABLE IF NOT EXISTS `tb_users` (
 
 -- Dumping data for table sinta.tb_users: ~6 rows (approximately)
 /*!40000 ALTER TABLE `tb_users` DISABLE KEYS */;
-INSERT IGNORE INTO `tb_users` (`id`, `ip_address`, `id_konsumen`, `username`, `password`, `salt`, `email`, `activation_code`, `forgotten_password_code`, `forgotten_password_time`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`, `avatar`) VALUES
+INSERT INTO `tb_users` (`id`, `ip_address`, `id_konsumen`, `username`, `password`, `salt`, `email`, `activation_code`, `forgotten_password_code`, `forgotten_password_time`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`, `avatar`) VALUES
 	(1, '127.0.0.1', 0, 'sintawulansr', '$2y$08$Kpso52jO/ld.bHeDSE5hPO2x9qsIdnVhEVlgFqrZ3c8TIq6zuNLze', '', 'sinta@admin.com', '86ed629d0fc67b65fa78a1f7b776dd9c56032abb', NULL, NULL, 'G.WaoqYoZ/Zq6l6VddiHGe', '0000-00-00 00:00:00', '2019-08-18 11:40:30', 1, 'Sintaaaaaaaaaaaaa', 'Wulansari', 'ANDROMEDA', '123445566778', 'user8-128x128.jpg'),
 	(9, '::1', 7, 'rizkifreaoo', '$2y$08$Kpso52jO/ld.bHeDSE5hPO2x9qsIdnVhEVlgFqrZ3c8TIq6zuNLze', NULL, 'rizkipebrianto96@bpr.co.id', NULL, NULL, NULL, 'fmAygl1DaHNc5qtHi/xHFO', '2018-11-09 16:55:31', '2019-08-12 04:22:23', 1, 'Rizki', 'Pebrianto', 'BPR Kertaharja', '08544432', 'user1-128x128.jpg'),
 	(19, '::1', 8, 'testgrup', '$2y$08$nKPUD5rmnZCczbAt6gtZkOPLtBNisJaEbQG5IRLk4KAtgWk28gF/.', NULL, 'testgrup@testgrup.com', NULL, NULL, NULL, NULL, '2019-08-08 07:32:44', NULL, 1, 'TEST', 'GRUP', '7', '123445566778', NULL),
@@ -266,9 +266,9 @@ CREATE TABLE IF NOT EXISTS `tb_users_groups` (
   CONSTRAINT `tb_users_groups_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `tb_users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=126 DEFAULT CHARSET=utf8;
 
--- Dumping data for table sinta.tb_users_groups: ~5 rows (approximately)
+-- Dumping data for table sinta.tb_users_groups: ~6 rows (approximately)
 /*!40000 ALTER TABLE `tb_users_groups` DISABLE KEYS */;
-INSERT IGNORE INTO `tb_users_groups` (`id`, `user_id`, `group_id`) VALUES
+INSERT INTO `tb_users_groups` (`id`, `user_id`, `group_id`) VALUES
 	(38, 1, 1),
 	(111, 9, 2),
 	(121, 19, 2),
